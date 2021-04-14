049b26e1c12a696874da72306d97ff958514ffd9
Harshini27 <harshinihoney27@gmail.com>
2021-04-14 22:05:23 +0530

    cloned

---
+++ 02-frontend/src/app/common/product.ts
@@ -0,0 +1,19 @@
+export class Product {
+
+    sku:string;
+    name:string;
+    description:string;
+    unitprice:number;
+    imageurl:string;
+    active:boolean;
+    unitsInStock:number;
+    dateCreated:Date;
+    lastUpdated:Date;
+
+
+
+
+
+
+}
+
